59c8da3c3a9eb03204b8a79bc2510b595c9c4425
zurustar <zurustar@users.noreply.github.com>
2018-06-02 21:10:53 +0900

    updated

--- NMT/nmt.py
+++ NMT/nmt.py
@@ -17,8 +17,13 @@ import subprocess
 import os
 import numpy as np
 
+import keras
 
-def load():
+#
+# NICTが公開している翻訳データを解析し、英語とそれに対応する日本語を抽出する
+#  同じフォルダに事前にUTF-8化した翻訳データがpara.utf8.txtという名前でおいておく必要あり
+#
+def load_for_nict():
 	mecab = 'mecab'
 	ja_utf8_txt = './ja.utf8.txt'
 	en_utf8_txt = './en.utf8.txt'
@@ -45,6 +50,10 @@ def load():
 	return en.stdout.decode(), ja.stdout.decode()
 
 
+#
+# 文字列を受け取り、出現する文字に一意の番号を付与したうえで、
+# 受け取った文字列を番号の並びに変換する
+#
 def generate_vocabulary(src):
 	special_characters = ['<PAD>', '<UNK>', '<GO>', '<EOS>']
 	vocabulary = {}
@@ -65,16 +74,34 @@ def generate_vocabulary(src):
 
 
 
-def conv2ary(en, ja):
-	en_vocabulary, en_array = generate_vocabulary(en)
-	ja_vocabulary, ja_array = generate_vocabulary(ja)
-	print(en_array)
-
-		
-
 def main():
-	en, ja = load()
-	conv2ary(en, ja)
+
+	# 学習対象の翻訳データを抜き出して、
+    # 学習用の形式に変換
+	en, ja = load_for_nict()
+	en_vocab, en_array = generate_vocabulary(en)
+	ja_vocab, ja_array = generate_vocabulary(ja)
+
+	# 入力の次元数を表すので、ボキャブラリ数に該当する
+	max_word_index = len(en_vocab)
+	if max_word_index < len(ja_vocab):
+		max_word_index =len(ja_vocab)
+
+	# Embeddingの結果、何次元のテンソルにしたいか？
+	output_dim = 64
+
+    # ？？
+	input_length = 10
+
+	model = keras.models.Sequential()
+	print("add embedding layer")
+	model.add(keras.layers.embeddings.Embedding(max_word_index, output_dim, input_length=input_length))
+	print("add encoder")
+	model.add(keras.layers.LSTM(64))
+	print("add decoder")
+	model.add(keras.layers.LSTM(64))
+	model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
 
 if __name__ == '__main__':
 	main()
