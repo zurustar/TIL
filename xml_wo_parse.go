5e4f4db814d39a85a98ebe64228385b92a9b203d
近江俊哉 <oumi@oumitoshiyanoMacBook-Air.local>
2019-05-12 09:40:48 +0900

    コメント追加

--- xml_wo_parse.go
+++ xml_wo_parse.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+// 要素を格納する変数名は必ず「大文字」で始める
+// スコープと関係があるのか？
 type Item struct {
 	Title   string    `xml:"title"`
 	PubDate time.Time `xml:"pubDate"`
@@ -20,18 +22,24 @@ type RSS struct {
 	Channel Channel `xml:"channel"`
 }
 
+// 時刻情報は直接time.Timeに格納してくれないので、
+// 専用の解析処理を書く必要がある。
 func (item *Item) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	// 一時的に名前データを格納する場所。
 	raw := struct {
 		Title   string `xml:"title"`
 		PubDate string `xml:"pubDate"`
 	}{}
+	// 一旦素の解析処理を呼び出して、日付は文字列で取得
 	if err := d.DecodeElement(&raw, &start); err != nil {
 		return err
 	}
+	// 文字列で撮ってきた時刻情報を個別にtime.Time化
 	t, err := time.Parse(time.RFC1123Z, raw.PubDate)
 	if err != nil {
 		return err
 	}
+	// 解析結果を格納。
 	*item = Item{
 		raw.Title,
 		t,
